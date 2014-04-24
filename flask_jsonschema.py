a2fc3a6db8fd468eeb437497d5c4610941aa680c
Martin Putniorz <mputniorz@gmail.com>
2014-04-24 14:47:20 +0200

    Other way around, only control on POST and PUT

--- flask_jsonschema.py
+++ flask_jsonschema.py
@@ -55,10 +55,9 @@ class JsonSchema(object):
         def wrapper(fn):
             @wraps(fn)
             def decorated(*args, **kwargs):
-                if request.method == 'GET':
-                    return fn(*args, **kwargs)
-                schema = current_app.extensions['jsonschema'].get_schema(path)
-                validate(request.json, schema)
+                if request.method in ('POST', 'PUT'):
+                    schema = current_app.extensions['jsonschema'].get_schema(path)
+                    validate(request.json, schema)
                 return fn(*args, **kwargs)
             return decorated
         return wrapper
